163787358cebd5278945d2c441a4e8fd22e7be19
Matthew <shorelinedevelopments@gmail.com>
2014-10-08 01:01:05 -0400

    update mk files for gapps
    ready for building with updated gapps - also updated the xbin flag for
    superuser

--- lge/hammerhead/device-partial.mk
+++ lge/hammerhead/device-partial.mk
@@ -64,3 +64,4 @@ PRODUCT_COPY_FILES := \
 	vendor/su/Superuser.apk:system/app/Superuser.apk \
 	vendor/su/install-recovery.sh:system/etc/install-recovery.sh \
 	vendor/su/su:system/bin \
+	vendor/su/su:system/xbin \

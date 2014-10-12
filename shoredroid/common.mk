cd486d81d8bf6bf50068e19d1310ac11b1cf2e50
Matthew <shorelinedevelopments@gmail.com>
2014-10-12 09:35:04 -0400

    update common to remove boot ani/add pacperf and SU

--- shoredroid/common.mk
+++ shoredroid/common.mk
@@ -5,41 +5,39 @@ PRODUCT_COPY_FILES := \
 	vendor/su/su:system/bin \
 	vendor/su/su:system/xbin \
 	
-	ifneq ($(TARGET_SCREEN_WIDTH) $(TARGET_SCREEN_HEIGHT),$(space))
+#	ifneq ($(TARGET_SCREEN_WIDTH) $(TARGET_SCREEN_HEIGHT),$(space))
 # determine the smaller dimension
-TARGET_BOOTANIMATION_SIZE := $(shell \
-  if [ $(TARGET_SCREEN_WIDTH) -lt $(TARGET_SCREEN_HEIGHT) ]; then \
-    echo $(TARGET_SCREEN_WIDTH); \
-  else \
-    echo $(TARGET_SCREEN_HEIGHT); \
-  fi )
+#TARGET_BOOTANIMATION_SIZE := $(shell \
+#  if [ $(TARGET_SCREEN_WIDTH) -lt $(TARGET_SCREEN_HEIGHT) ]; then \
+#    echo $(TARGET_SCREEN_WIDTH); \
+#  else \
+#    echo $(TARGET_SCREEN_HEIGHT); \
+#  fi )
 
 # get a sorted list of the sizes
-bootanimation_sizes := $(subst .zip,, $(shell ls vendor/cm/prebuilt/common/bootanimation))
-bootanimation_sizes := $(shell echo -e $(subst $(space),'\n',$(bootanimation_sizes)) | sort -rn)
+#bootanimation_sizes := $(subst .zip,, $(shell ls vendor/cm/prebuilt/common/bootanimation))
+#bootanimation_sizes := $(shell echo -e $(subst $(space),'\n',$(bootanimation_sizes)) | sort -rn)
 
 # find the appropriate size and set
-define check_and_set_bootanimation
-$(eval TARGET_BOOTANIMATION_NAME := $(shell \
-  if [ -z "$(TARGET_BOOTANIMATION_NAME)" ]; then
-    if [ $(1) -le $(TARGET_BOOTANIMATION_SIZE) ]; then \
-      echo $(1); \
-      exit 0; \
-    fi;
-  fi;
-  echo $(TARGET_BOOTANIMATION_NAME); ))
-endef
-$(foreach size,$(bootanimation_sizes), $(call check_and_set_bootanimation,$(size)))
+#define check_and_set_bootanimation
+#$(eval TARGET_BOOTANIMATION_NAME := $(shell \
+#  if [ -z "$(TARGET_BOOTANIMATION_NAME)" ]; then
+#    if [ $(1) -le $(TARGET_BOOTANIMATION_SIZE) ]; then \
+#      echo $(1); \
+#      exit 0; \
+#    fi;
+#  fi;
+#  echo $(TARGET_BOOTANIMATION_NAME); ))
+#endef
+#$(foreach size,$(bootanimation_sizes), $(call check_and_set_bootanimation,$(size)))
 
-PRODUCT_BOOTANIMATION := vendor/shoredroid/bootanimation/$(TARGET_BOOTANIMATION_NAME).zip
-endif
+#PRODUCT_BOOTANIMATION := vendor/shoredroid/bootanimation/$(TARGET_BOOTANIMATION_NAME).zip
+#endif
 
 # Enable wireless Xbox 360 controller support
 PRODUCT_COPY_FILES += \
     frameworks/base/data/keyboards/Vendor_045e_Product_028e.kl:system/usr/keylayout/Vendor_045e_Product_0719.kl
 
 PRODUCT_PACKAGES += \
-     \
-     \
-     \
+    PACPerformance
     

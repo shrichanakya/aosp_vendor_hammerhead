7a86a28d731013277984e6c3ee9ba31a2a2d4506
Matthew <m.hanhimaki@gmail.com>
2014-10-14 16:55:59 -0400

    remove xbin copy

--- shoredroid/common.mk
+++ shoredroid/common.mk
@@ -3,7 +3,7 @@ PRODUCT_COPY_FILES := \
 	vendor/su/Superuser.apk:system/app/Superuser.apk \
 	vendor/su/install-recovery.sh:system/etc/install-recovery.sh \
 	vendor/su/su:system/bin \
-	vendor/su/su:system/xbin \
+#	vendor/su/su:system/xbin \
 	
 #	ifneq ($(TARGET_SCREEN_WIDTH) $(TARGET_SCREEN_HEIGHT),$(space))
 # determine the smaller dimension

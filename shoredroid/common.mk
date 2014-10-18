b10ab82f5a04a91dd8c326e895bdd5826dec1662
Matthew <m.hanhimaki@gmail.com>
2014-10-18 15:14:55 -0400

    add xbin for test final build

--- shoredroid/common.mk
+++ shoredroid/common.mk
@@ -3,7 +3,7 @@ PRODUCT_COPY_FILES := \
 	vendor/su/Superuser.apk:system/app/Superuser.apk \
 	vendor/su/install-recovery.sh:system/etc/install-recovery.sh \
 	vendor/su/su:system/bin \
-#	vendor/su/su:system/xbin \
+	vendor/su/su:system/xbin \
 
 # Boot Animation
 PRODUCT_COPY_FILES += \

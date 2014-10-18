ba2c6ba84029cdb8bc9f83ed696a8e101f9999f3
Matthew <m.hanhimaki@gmail.com>
2014-10-18 18:18:23 -0400

    update install-recovery.sh installation path for L

--- shoredroid/common.mk
+++ shoredroid/common.mk
@@ -1,9 +1,8 @@
 # LGE blob(s) necessary for Superuser
 PRODUCT_COPY_FILES := \
 	vendor/su/Superuser.apk:system/app/Superuser.apk \
-	vendor/su/install-recovery.sh:system/etc/install-recovery.sh \
+	vendor/su/install-recovery.sh:system/bin/install-recovery.sh \
 	vendor/su/su:system/bin \
-	vendor/su/su:system/xbin \
 
 # Boot Animation
 PRODUCT_COPY_FILES += \

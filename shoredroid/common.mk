307636fb12df679cc1d9287f0074531b5f299536
shoredroid <shoreholdingsinc@gmail.com>
2014-10-21 23:21:11 -0400

    adding ViPER

--- shoredroid/common.mk
+++ shoredroid/common.mk
@@ -17,7 +17,7 @@ PRODUCT_COPY_FILES := \
 	vendor/su/Superuser.apk:system/app/Superuser.apk \
 	vendor/su/install-recovery.sh:system/etc/install-recovery.sh \
 	vendor/su/su:system/bin \
-	vendor/su/su:system/xbin \
+#       vendor/su/su:system/xbin 
 
 # Boot Animation
 PRODUCT_COPY_FILES += \
@@ -26,6 +26,11 @@ PRODUCT_COPY_FILES += \
 # Enable wireless Xbox 360 controller support
 PRODUCT_COPY_FILES += \
     frameworks/base/data/keyboards/Vendor_045e_Product_028e.kl:system/usr/keylayout/Vendor_045e_Product_0719.kl
+
+# ViPER
+PRODUCT_COPY_FILES += \
+    vendor/shoredroid/shoreaudio/libv4a_fx_kk_NEON.so:system/lib/soundfx/libv4a_fx_kk_NEON.so \
+    vendor/shoredroid/shoreaudio/ViPER4Android_FX_A4.x.apk:system/app/ViPER4Android_FX_A4.x.apk
 
 PRODUCT_PACKAGES += \
     PACPerformance

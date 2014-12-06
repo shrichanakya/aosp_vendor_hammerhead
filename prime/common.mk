6e91fb9c98164d14046b32dcb0fed8c772a528c6
Matthew <m.hanhimaki@gmail.com>
2014-12-05 19:52:42 -0500

    Update common.mk

--- prime/common.mk
+++ prime/common.mk
@@ -34,7 +34,7 @@ PRODUCT_COPY_FILES += \
 # Build Properties for Prime
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.rommanager.developerid=shoredroid \
-    ro.modversion=Prime_Weekly_20141212
+    ro.modversion=Prime_Bleeding_20141205
     ro.config.ringtone=RollOut.ogg
     ro.config.notification_sound=Autobots.ogg
 

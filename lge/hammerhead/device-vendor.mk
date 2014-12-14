34e7f272d68b3ac4e0dbaaa7e26a9fff7aee71cb
barracuda7 <barracudaking@gmail.com>
2014-12-14 11:57:58 +1100

    remove prime

--- lge/hammerhead/device-vendor.mk
+++ lge/hammerhead/device-vendor.mk
@@ -20,4 +20,3 @@ LOCAL_STEM := hammerhead/device-partial.mk
 $(call inherit-product-if-exists, vendor/broadcom/$(LOCAL_STEM))
 $(call inherit-product-if-exists, vendor/lge/$(LOCAL_STEM))
 $(call inherit-product-if-exists, vendor/qcom/$(LOCAL_STEM))
-$(call inherit-product-if-exists, vendor/prime/primeinjection/injection.mk)
